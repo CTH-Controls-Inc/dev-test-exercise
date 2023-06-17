beedf20a79eddb7d963d4f451f93c90b02b6eb2b
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-06-16 21:20:11 -0400

    added email and password textfields

--- login-page/src/components/Login.jsx
+++ login-page/src/components/Login.jsx
@@ -3,6 +3,7 @@ import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
 import { lightBlue } from '@mui/material/colors';
 import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
+import TextField from '@mui/material/TextField';
 
 function Login() {
   const paperstyle = {
@@ -20,6 +21,27 @@ function Login() {
             <LoginTwoToneIcon />
           </Avatar>
           <h2>Sign In</h2>
+        </Grid>
+        <Grid spacing={2.5} direction={'column'} container>
+          <Grid item>
+            <TextField
+              required
+              id='outlined-required'
+              label='Email'
+              placeholder='john@doe.com'
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id='outlined-required'
+              label='Password'
+              placeholder='********'
+              type='password'
+              fullWidth
+            />
+          </Grid>
         </Grid>
       </Paper>
     </Grid>
