56ec25c9ed78f44e6d8c928cc9601f55de564ab5
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-06-16 20:44:36 -0400

    added the login icon

--- login-page/src/components/Login.jsx
+++ login-page/src/components/Login.jsx
@@ -1,5 +1,8 @@
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import { lightBlue } from '@mui/material/colors';
+import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
 
 function Login() {
   const paperstyle = {
@@ -12,7 +15,12 @@ function Login() {
   return (
     <Grid>
       <Paper elevation={3} style={paperstyle}>
-        Sign In
+        <Grid align='center'>
+          <Avatar sx={{ bgcolor: lightBlue[500] }}>
+            <LoginTwoToneIcon />
+          </Avatar>
+          <h2>Sign In</h2>
+        </Grid>
       </Paper>
     </Grid>
   );
