db3e318e537e2c03d89b5270b15fd56a1f45426b
Gozi1 <82515026+Gozi1@users.noreply.github.com>
2023-06-16 22:28:13 -0400

    Add Page Hmtl/CSS Layout

--- dev-test-next-app/src/app/page.js
+++ dev-test-next-app/src/app/page.js
@@ -4,7 +4,19 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <div className={styles['login-container']}>
+					<h1>Login</h1>
+					<input
+						className={styles['login-input']}
+						type='email'
+						placeholder='E-mail'
+					/>
+					<input
+						className={styles['login-input']}
+						type='password'
+						placeholder='Password'
+					/>
+				</div>
     </main>
   )
 }
