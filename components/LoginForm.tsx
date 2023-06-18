79c41b7c4839e70d462d9e22d977fdeb71b83d28
bcmendis <benclintonmendis@gmail.com>
2023-06-18 18:40:41 -0400

    Added react-hook-form integration

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,7 +1,51 @@
 'use client'
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  email: string;
+};
+
+// Email validation Regular Expression as per W3C specification
+const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  // Initializing React-Hook-Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+    },
+    mode: "all",
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log("Form Values:", data);
+  };
+
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <h1>Login</h1>
+      <label htmlFor="login">Email</label>
+      <input
+        id="email"
+        type="email"
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: regex,
+            message: "Invalid email format",
+          },
+        })}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default LoginForm;
