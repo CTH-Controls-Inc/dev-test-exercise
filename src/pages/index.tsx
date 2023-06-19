356ced5a2600e09ecad16dfacf592576db06c2c1
Анна Корниенко <akornienko94@gmail.com>
2023-06-19 17:46:44 -0400

    Created React App using CRA

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div>
+          <p>
+              Login&nbsp;
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
