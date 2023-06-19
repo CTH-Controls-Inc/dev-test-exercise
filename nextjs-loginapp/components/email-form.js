830ad16c36c56886ffc890c4b84ebcc53ad8bc1d
cd155 <cd155@163.com>
2023-06-19 17:38:26 -0400

    add login page

--- nextjs-loginapp/components/email-form.js
+++ nextjs-loginapp/components/email-form.js
@@ -2,6 +2,21 @@ import styles from '../styles/email.module.css';
 
 export default function LoginForm() {
   return (
-    <h1>test</h1>
+    <div>
+      <form>
+        <h2>CTH Controls Exercise: Please enter your email</h2>
+        <label>
+          Email:
+          <input type="email" name="email"/>
+        </label>
+        <br />
+        <br />
+        <label>
+         Password:
+         <input type="text" name="password" required />
+       </label>
+        <h3>Result: </h3>
+      </form>
+    </div>
   )
 }
