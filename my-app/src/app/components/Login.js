21d662af4695fd08e25849cf53ad8a2397315bba
billfern23 <billfern23@gmail.com>
2023-06-22 19:47:59 -0400

    "copied boiler plate code from boostrap"

--- my-app/src/app/components/Login.js
+++ my-app/src/app/components/Login.js
@@ -1,11 +1,23 @@
-
 "use client";
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
