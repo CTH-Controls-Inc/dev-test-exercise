f302b90a138deb10be14168ef5a71e54427fc9d9
billfern23 <billfern23@gmail.com>
2023-06-22 19:43:59 -0400

    "added component Login"

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Login from "./components/Login"
 
 export default function Home() {
   return (
 
       <main >
-     
-    </main>
+        <Login />
+      </main>
   )
 }
