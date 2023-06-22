5659a80d13e2775666dc2f13044dba7c99f3cd98
billfern23 <billfern23@gmail.com>
2023-06-22 19:52:21 -0400

    "moved form to center"

--- my-app/src/app/components/Login.js
+++ my-app/src/app/components/Login.js
@@ -3,19 +3,41 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import {AiOutlineExclamationCircle} from "react-icons/ai";
+import { useState } from "react";
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
+        <div style={{
+            height:"100vh",
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+   <Form>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Label column sm="2">
+                Email
+                </Form.Label>
+                <Col sm="10">
+                <Form.Control plaintext  defaultValue="email@example.com" />
+                </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                Password
+                </Form.Label>
+                <Col sm="10">
+                <Form.Control type="password" placeholder="Password" />
+                </Col>
+            </Form.Group>
+        <Button>Submit</Button>
       </Form>
+
+        </div>
+   
     </div>
   );
 };
